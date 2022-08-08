14289674bca7b168186816e8c68c4dc7d8848c42
D.Zubkov <zda3171@gmail.com>
2022-08-08 18:34:04 +0300

    Run test

--- src/test/java/ru/netology/RequestDebitCardTest.java
+++ src/test/java/ru/netology/RequestDebitCardTest.java
@@ -1,4 +1,51 @@
 package ru.netology;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class RequestDebitCardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setupAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\daimo\\IdeaProjects\\DZ21Netology\\Drivers\\Win\\chromedriver.exe");
+    }
+
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestv1() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Николай Пупов");
+        elements.get(1).sendKeys("+79000000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+
+        assertEquals(expected, actual);
+
+
+    }
 }
