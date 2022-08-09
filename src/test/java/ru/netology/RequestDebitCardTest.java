3b2d7a2b4e5128eb1f112edec91868ad10aad66a
D.Zubkov <zda3171@gmail.com>
2022-08-09 12:50:29 +0300

    add HEADLESS

--- src/test/java/ru/netology/RequestDebitCardTest.java
+++ src/test/java/ru/netology/RequestDebitCardTest.java
@@ -1,23 +1,22 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import java.util.List;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ChromeTest {
 
     WebDriver driver;
+
+
 
     @BeforeAll
     static void setupAll() {
@@ -26,7 +25,11 @@ class ChromeTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @AfterEach
@@ -40,13 +43,12 @@ class ChromeTest {
     void shouldTestv1() throws InterruptedException {
         driver.get("http://localhost:9999");
         Thread.sleep(5000);
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Николай Пупов");
-        elements.get(1).sendKeys("+79000000000");
-        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Николай Пупов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79000000000");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button")).click();
-        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
 
         assertEquals(expected, actual);
 
